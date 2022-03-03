9b0d37eb914ef98bf0749b551b78d794234501cd
Ali <tu8cvbxnx@code.edu.az>
2022-03-03 20:18:38 +0400

    Tasks

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+                Console.WriteLine("Odd number");
+            else
+                Console.WriteLine("An even number");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first number: ");
+            int firstNum,secondNum;
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+
+            if (firstNum > secondNum)
+                Console.WriteLine("first number greatest than second");
+            else if (firstNum == secondNum)
+                Console.WriteLine("first number is equal the second");
+            else
+            {
+                Console.WriteLine("Second number greatest than first");
+            }
+        }
+    }
+}
